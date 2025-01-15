3cc11fe709023a57cd129cdeb45067ca9fe9bd25
Obikobe Amaobi <aobikobe@gmail.com>
2025-01-15 09:59:21 +0100

    add store

---
+++ db/sqlc/store.go
@@ -0,0 +1,90 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+// we are using composition instead of inheritance to extend struct functionality
+// Queries struct does not support transaction
+type Store struct {
+	*Queries // all queries are available to store
+	db       *sql.DB
+}
+
+// creates a new Store
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
+}
+
+// executes a function within a database
+
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.db.BeginTx(ctx, nil)
+
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+
+	err = fn(q)
+
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx err: %w, rb err: %w", err, rbErr)
+		}
+
+		return err
+	}
+
+	return tx.Commit()
+}
+
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+type TransferTxResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Account  `json:"from_entry"`
+	ToEntry     Account  `json:"to_entry"`
+}
+
+func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
+
+
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID: arg.ToAccountID,
+			Amount: arg.Amount,
+		})
+
+		if err != nil {
+			return  err
+		}
+		result.FromEntry, err = q.CreateEntry( ctx, CreateEntryParams{
+			AccountID: arg.FromAccountID,
+			Amount: -arg.Amount,
+		})
+
+		if err != nil {
+			return  err
+		}
+		return nil
+	})
+
+	return result, err
+}
